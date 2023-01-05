32603125c0839f45eb7e2305fefe9385f5b4c8c6
lapi7 <lapi777@gmail.com>
2023-01-05 17:42:58 +0100

    Main view :)

--- app/src/main/java/com/example/pwta_proj2/MainActivity.java
+++ app/src/main/java/com/example/pwta_proj2/MainActivity.java
@@ -1,32 +1,55 @@
 package com.example.pwta_proj2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.app.AlertDialog.Builder;
+import android.app.AlertDialog;
 import java.io.File;
 
+import com.dinuscxj.gesture.MultiTouchGestureDetector;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.SceneView;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
+import java.net.URI;
 import java.util.concurrent.CompletableFuture;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     public String filePath = "";
+    private MultiTouchGestureView imgView;
+    AlertDialog.Builder builder;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MultiTouchGestureView imgView;
+
     }
 
     int requestCode = 1;
@@ -40,15 +63,11 @@ public class MainActivity extends AppCompatActivity {
             }
 
             Uri uri = data.getData();
-            //filePath = uri.getPath();
+
             filePath = Utils.getNameFromURI(context, uri);
             // TODO: Include full path with filename
 
-            TextView textViewStatus = findViewById(R.id.textViewStatus);
-            textViewStatus.setText(filePath);
 
-
-            //Toast.makeText(context,uri.getPath(), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -60,4 +79,50 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, requestCode);
     }
 
+
+    public void select2DModel(View view) {
+        Context context = getApplicationContext();
+        String[] listItems = new String[]{"JPEG File", "BMP File", "PNG File"};
+        final int[] checkedItem = {-1};
+
+        builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("2D Model");
+
+        builder.setSingleChoiceItems(listItems, -1, (dialog, which) -> {
+            checkedItem[0] = which;
+            TextView textViewStatus = findViewById(R.id.textViewStatus);
+            textViewStatus.setText(listItems[which]);
+
+            imgView = findViewById(R.id.imageView);
+
+            switch (listItems[which]) {
+                case "JPEG File":
+                    imgView.setImage(context.getResources().getDrawable(R.drawable.androidjpeg));
+                    break;
+                case "BMP File":
+                    imgView.setImage(context.getResources().getDrawable(R.drawable.androidbmp));
+                    break;
+                case "PNG File":
+                    imgView.setImage(context.getResources().getDrawable(R.drawable.androidpng));
+                    break;
+                default:
+                    dialog.dismiss();
+            }
+
+            dialog.dismiss();
+        }).setNegativeButton("Cancel", (dialog, which) -> {
+
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+
+
+
+
 }
+
+
